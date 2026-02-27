5910314f609149f0269d8503432919bfc15224ca
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-02-27 17:46:44 +0200

    end content unlocked

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import http from 'node:http';
 
 const PORT = 8000;
 const server = http.createServer((req, res) =>{
-    res.write("Here is your information")
-    res.wri
+    res.write("Here is your information \n")
+    res.write("More information\n")
     res.end("Waramutse Rwanda", "utf8", () => console.log("Response provided"));
 });
 
