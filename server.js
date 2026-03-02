f8030d4cc355d3751ab6a494b4e87c1d52142af2
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-02 18:31:33 +0200

    adding content type

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import http from 'node:http'
-import { getDataFromDB } from './database/db.js'
+import { getDataFromDB } from './db.js'
 
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB();
     if (req.url === '/api' && req.method === 'GET') {
+
+        res.setHeader("Content-Type", "application/json")
+        res.statusCode = 200
         res.end(JSON.stringify(destinations));
     }
 })
