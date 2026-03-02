495951f8954b1685094dc58b26bf6d6cb478212f
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-02 16:16:50 +0200

    Routing and req object

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ import http from 'node:http';
 
 const PORT = 8000;
 const server = http.createServer((req, res) =>{
-    res.write("Here is your information \n")
-    res.write("More information\n")
-    res.end("Waramutse Rwanda", "utf8", () => console.log("Response provided"));
+    if(req.url === "/api" && req.method === "GET") res.end("Waramutse Rwanda");
+    res.end("Kabaye")
 });
 
 server.listen(PORT, () => console.log("Server is running on: " + PORT));
