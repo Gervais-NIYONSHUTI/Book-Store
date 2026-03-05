097a5b7fd8778ab083cb6f17ba5e2a3727dd29df
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-05 07:47:32 +0200

    path resource

--- Routes&Paths/server.js
+++ Routes&Paths/server.js
@@ -1,13 +1,18 @@
+import path from 'node:path'
 import http from 'node:http'
 
 const PORT = 8000
 
+const __dirname = import.meta.dirname
+
 const server = http.createServer((req, res) => {
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><h1>The server is working</h1></html>')
-    
+  const pathToResource = path.join(__dirname, 'public', 'index.html')
+
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end()
+
 })
 
-server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
+server.listen(PORT, () => console.log('connected on port 8000'))
