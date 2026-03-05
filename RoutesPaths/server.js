24d7373d7f4b80af38dd00fe6b73c645ed8c840d
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-05 08:05:28 +0200

    fs module

--- RoutesPaths/server.js
+++ RoutesPaths/server.js
@@ -1,17 +1,20 @@
 import path from 'node:path'
 import http from 'node:http'
+import fs from 'node:fs/promises'
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
   const pathToResource = path.join(__dirname, 'public', 'index.html')
 
+  const content = await fs.readFileSync(pathToResource)
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end()
+  res.end(content)
 
 })
 
