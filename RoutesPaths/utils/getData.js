ec59f1ba2f1d23c6b7bbbe15f8d7726d1d600704
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-06 18:25:59 +0200

    wire up api

--- RoutesPaths/utils/getData.js
+++ RoutesPaths/utils/getData.js
@@ -3,7 +3,7 @@ import fs from 'node:fs/promises'
 
 export async function getData() {
     try{
-        const pathJSON = path.join('data', 'data.json')
+        const pathJSON = path.join('./RoutesPaths/data', 'data.json')
         const data = await fs.readFile(pathJSON, 'utf8')
         const parsedData = JSON.parse(data)
         return parsedData
@@ -11,17 +11,4 @@ export async function getData() {
         console.log(err)
         return []
     }
-/*
-Challenge:
-1. getData() should: 
-    - read the json in json.data as a string 
-    - parse it to JS 
-    - return the parsed data. 
-
-   If there’s an error, it should return an empty array (think, why are we doing this?).
-
-hint.md for help
-*/
-
-  return 'I am from getData()!'
 }
