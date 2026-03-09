84e2f9705dcf50c4ea2c258b7e29673955ed9c44
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-09 08:52:14 +0200

    event emitter

--- RoutesPaths/handlers/routeHandlers.js
+++ RoutesPaths/handlers/routeHandlers.js
@@ -3,8 +3,9 @@ import { sendResponse } from '../utils/sendResponse.js'
 import { parseJSONBody } from '../utils/parseJSONBody.js'
 import { addNewSighting } from '../utils/addNewSighting.js'
 import { sanitizeInput } from '../utils/sanitizeInput.js'
+import { sightingEvents } from '../events/sightingEvents.js'
 
-export async function handleGet(res) {
+export async function handleGet(res) { 
   const data = await getData()
   const content = JSON.stringify(data)
   sendResponse(res, 200, 'application/json', content)
@@ -16,9 +17,9 @@ export async function handlePost(req, res) {
     const parsedBody = await parseJSONBody(req)
     const sanitizedBody = sanitizeInput(parsedBody)
     await addNewSighting(sanitizedBody)
+    sightingEvents.emit('sighting-added', sanitizedBody)
     sendResponse(res, 201, 'application/json', JSON.stringify(sanitizedBody))
   } catch (err) {
-    sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
+    sendResponse(res, 400, 'application/json', JSON.stringify({ error: err }))
   }
-
 }
